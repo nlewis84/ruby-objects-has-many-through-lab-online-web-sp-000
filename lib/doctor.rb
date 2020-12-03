092fb59269ea08ba15eee98b113731a365d51241
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-03 05:48:28 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Doctor
+  
+  attr_accessor :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    date = Appointment.new(date, self, patient)
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+    binding.pry
+  end
+  
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
+end
